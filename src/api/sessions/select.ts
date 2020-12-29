9350258c7f90b3f8e645f8f9310cc9804ac36b61
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-12-29 16:30:33 +0100

    error (SQL error near zbek)

--- src/api/sessions/select.ts
+++ src/api/sessions/select.ts
@@ -4,67 +4,19 @@ const router = express.Router();
 
 router.get('/', (req: Request, res: Response, _next: Next) =>
 {
-    const isEmpty = (value) => {
-        // tslint:disable-next-line:prefer-const only-arrow-functions
-        let isEmptyObject = function(a) {
-            if (typeof a.length === 'undefined') { // it's an Object, not an Array
-                // tslint:disable-next-line:prefer-const
-                let hasNonempty = Object.keys(a).some(function nonEmpty(element){
-                    return !isEmpty(a[element]);
-                });
-                return hasNonempty ? false : isEmptyObject(Object.keys(a));
-            }
+    let _SQL = `SELECT * FROM sessions
+            WHERE fingerprint='${req.query.fingerprint || null}'
+            ORDER BY session_id DESC
+            LIMIT ${req.query.limit || -1}
+            OFFSET ${req.query.offset || 0}`;
 
-            return !a.some(function nonEmpty(element) {
-                return !isEmpty(element);
-            });
-        };
-        return (
-            // tslint:disable-next-line:triple-equals
-            value == false
-            || typeof value === 'undefined'
-            || value == null
-            || (typeof value === 'object' && isEmptyObject(value))
-        );
-    };
-
-    let _SQL = `SELECT * from sessions ORDER BY session_id DESC LIMIT -1 OFFSET 0`;
-
-    if(isEmpty(req.query.fingerprint) || !req.query.fingerprint)
-    {
-        res.status(500).json({
-            code: 500,
-            message: 'fingerprint param is required',
-            isSuccess: false,
-            data: []
-        }).end();
-    }
-
-    if(req.query.fingerprint)
+    if(req.query.session)
     {
         _SQL = `SELECT * FROM sessions
-            WHERE fingerprint='${encodeURI(req.query.fingerprint)}'
+            WHERE session='${req.query.session || null}' AND fingerprint='${req.query.fingerprint || null}'
             ORDER BY session_id DESC
-            LIMIT ${encodeURI(req.query.limit) || -1}
-            OFFSET ${encodeURI(req.query.offset) || 0}`;
-    }
-
-    if(req.query.session && req.query.fingerprint)
-    {
-        if(isEmpty(req.query.session))
-        {
-            res.status(500).json({
-                code: 500,
-                message: 'session param is required',
-                isSuccess: false,
-                data: []
-            }).end();
-        }
-        _SQL = `SELECT * FROM sessions
-            WHERE session='${encodeURI(req.query.session)}' AND fingerprint='${encodeURI(req.query.fingerprint)}'
-            ORDER BY session_id DESC
-            LIMIT ${encodeURI(req.query.limit) || -1}
-            OFFSET ${encodeURI(req.query.offset) || 0}`;
+            LIMIT ${req.query.limit || -1}
+            OFFSET ${req.query.offset || 0}`;
     }
 
     db.serialize(()=> {
